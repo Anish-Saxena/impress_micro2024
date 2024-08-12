60f36d274d98318afdb049273bc03d108e813dd1
Anish Saxena <asaxena317@login-phoenix-slurm-4.pace.gatech.edu>
2024-08-11 20:04:36 -0400

    updated champsim test script

--- champsim/test_setup.sh
+++ champsim/test_setup.sh
@@ -5,9 +5,9 @@ DRAMSIM3_PATH=`pwd`
 cd ../champsim
 export LD_LIBRARY_PATH=$DRAMSIM3_PATH:$LD_LIBRARY_PATH
 cd bin
-./8C_16WLLC --simulation_instruction=100000 \
+./baseline --simulation_instruction=100000 \
 --warmup_instructions=1000000 \
--traces ../../experiments/traces/602.gcc_s-1850B.champsimtrace.xz | tee test_output.txt
+-traces ../../pythia/traces/602.gcc_s-1850B.champsimtrace.xz | tee test_output.txt
 if cat test_output.txt | grep -q "ChampSim completed all CPUs"; then
     echo "SETUP TESTED SUCCESSFULLY"
 else
